b5bb791760299e0c62fcad18633bff3898a6a3ee
Kranthi Paidi <kk.paidi@outlook.com>
2024-07-18 19:20:11 -0700

    initial commit

---
+++ src/main/java/com/ecommercedemo/product_service/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.ecommercedemo.product_service.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ecommercedemo.product_service.model.Product;
+
+public interface ProductRepository extends MongoRepository<Product, String>{
+
+}
---
+++ src/test/java/com/ecommercedemo/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,64 @@
+package com.ecommercedemo.product_service;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.ecommercedemo.product_service.dto.ProductRequest;
+import com.ecommercedemo.product_service.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductServiceApplicationTests {
+	
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+	
+	@Test
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+		Assertions.assertTrue(productRepository.findAll().size() == 1);
+
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("Product 1")
+				.description("Product 1 description")
+				.price(new BigDecimal("100.00"))
+				.build();
+	}
+
+}
